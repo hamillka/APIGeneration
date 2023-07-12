41733564f3e653230f0a1159a20d498aa5dc32e6
pichupido <ilyasov060@gmail.com>
2023-07-12 20:05:25 +0300

    Сделано: парсинг строки без вложенности и корректное сохранение объектов в List<GenericObject>

--- src/main/kotlin/api/parser/parser.kt
+++ src/main/kotlin/api/parser/parser.kt
@@ -1,8 +1,9 @@
 package api.parser
 
+import api.generic.GenericAttribute
+import api.generic.GenericObject
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
-
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
 
@@ -34,16 +35,158 @@ object UnknownUserSerializer : KSerializer<List<UnknownUser>> {
     }
 }
 
+data class NestedObject(val className: String, val details: JsonObject)
+
+// RECURSIVE//////
+//object NestedObjectSerializer : KSerializer<List<NestedObject>> {
+//    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("NestedObject") {
+//        element<JsonElement>("className")
+//        element<JsonElement>("details")
+//    }
+//
+//    override fun deserialize(decoder: Decoder): List<NestedObject> {
+//        val jsonInput = decoder as? JsonDecoder ?: error("Can be deserialized only by JSON")
+//        val json = jsonInput.decodeJsonElement().jsonObject
+//
+//        val details = json.toMutableMap()
+//
+//        val nestedList = mutableListOf<NestedObject>()
+//        nestedList.add(NestedObject("TEST", json))
+//
+//        for (element in details) {
+////            println("KEY=${element.key} VALUE=${element.value} TYPE=${element.value::class.simpleName}")
+//            if (element.value::class.simpleName == "JsonObject") {
+//                val nesObj = NestedObject(element.key, element.value.jsonObject)
+//                val nesObjs = Json.decodeFromString(NestedObjectSerializer, string = nesObj.details.toString())
+//                for (obj in nesObjs)
+//                    nestedList.add(obj)
+//            }
+//        }
+//        return nestedList
+//    }
+//
+//    override fun serialize(encoder: Encoder, value: List<NestedObject>) {
+//        error("Serialization is not implemented")
+//    }
+//}
+
+
+// NOT RECURSIVE
+object NestedObjectSerializer : KSerializer<NestedObject> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("NestedObject") {
+        element<JsonElement>("className")
+        element<JsonElement>("details")
+    }
+
+    override fun deserialize(decoder: Decoder): NestedObject {
+        val jsonInput = decoder as? JsonDecoder ?: error("Can be deserialized only by JSON")
+        val className = jsonInput.decodeJsonElement().toString()
+
+        val json = jsonInput.decodeJsonElement().jsonObject
+
+        val details = json.toMutableMap()
+
+        return NestedObject(className, JsonObject(details))
+    }
+
+    override fun serialize(encoder: Encoder, value: NestedObject) {
+        error("Serialization is not implemented")
+    }
+}
+
+
 fun main() {
+    /*
+    TESTS:
+        "users": [{"id":15,"name":"Andrey","Country": "Russia"}, {"id": 22, "name": "Mikhail", "age": 51, "address":  {"st.": "Lenina", "dom":  "55A"}}]
+        "users": [{"id":15,"name":"Andrey","Country": "Russia"}, {"id": 22, "name": "Mikhail", "age": 51, "address":  {"st.": "Lenina", "dom":  "55A"}}, {"id": 58, "vuz": "BMSTU"}]
+        "users": [{"id":15,"name":"Andrey","Country": "Russia"}, {"id": 22,"name": "Mikhail", "age": 51, "address": {"st.": "Lenina", "dom":  {"first": "55A", "second": "99G"}}}, {"id": 58, "vuz": "BMSTU"}] // FOR RECURSIVE
+
+        "users": [{"id":15,"name":"Andrey","ADDRESS": {"country": "Russia", "city": "moscow", "rayon": "SVAO", "dom": 91}}, {"id": 22, "name": "Mikhail", "age": 51, "address":  {"st.": "Lenina", "dom":  "55A"}}, {"id": 58, "vuz": "BMSTU"}]
+    */
+
+    val jsonStr: String = readln()
+
+    val (index, className) = getTableName(jsonStr)
+
     val objs = Json.decodeFromString(
         UnknownUserSerializer,
-        """[{"id":15,"name":"Andrey","Country": "Russia"}, {"id": 22, "name": "Mikhail", "age": 51, "address":  {"st.":  "Lenina", "dom":  "55A"}}]"""
+        string = jsonStr.slice(index + 1 until jsonStr.length),
     )
 
+    val genObjs = mutableListOf<GenericObject>()
+
     for (obj in objs) {
-        print("name=id\t\t\t    type=${obj.id::class.simpleName}\t\t\t  value=${obj.id}\n")
+        val genObj = GenericObject(className)
+        val idAttrib = GenericAttribute(name = "id", type = "Int", value = obj.id)
+        genObj.addAttribute(idAttrib)
+
         for (element in obj.details.keys) {
-            print("name=${element}\t\t\t    type=${obj.details[element]!!::class.simpleName}\t\t\t  value=${obj.details[element]}\n")
+            var type: String? = obj.details[element]!!::class.simpleName
+
+            when (type) {
+                "JsonObject" -> {
+//                    val nestedObj = NestedObject(element, obj.details[element] as JsonObject)
+                    val str = element + obj.details[element].toString()
+                    val nestedObj = Json.decodeFromString(
+                        NestedObjectSerializer,
+                        string = str
+                    ) // string = nestedObj.details.toString()
+                    val nestedGenObj = GenericObject(nestedObj.className)
+                    for (elem in nestedObj.details.keys) {
+                        nestedGenObj.addAttribute(
+                            GenericAttribute(
+                                name = elem,
+                                type = checkIntOrString(nestedObj.details[elem].toString()),
+                                value = nestedObj.details[elem]
+                            )
+                        )
+                    }
+                    genObj.addAttribute(
+                        GenericAttribute(
+                            name = nestedObj.className,
+                            type = "GenericObject",
+                            value = nestedGenObj
+                        )
+                    )
+                }
+
+                else -> type = checkIntOrString(obj.details[element].toString())
+            }
+
+            genObj.addAttribute(GenericAttribute(name = element, type = type, value = obj.details[element]))
         }
+        genObjs.add(genObj)
+    }
+
+
+    println("GEN_OBJS LIST")
+    for (i in 0 until genObjs.size) {
+        (genObjs[i].getAttributes()).forEach {
+            println("NAME=${it.name} TYPE=${it.type} VALUE=${it.value} ")
+            if (it.type == "GenericObject") {
+                println("Nested Object")
+                val ral = it.value as GenericObject
+                for (elem in ral.getAttributes()) {
+                    println("${elem.name} ${elem.type} ${elem.value}")
+                }
+            }
+        }
+        println()
+    }
+}
+
+
+fun getTableName(str: String): Pair<Int, String> {
+    val index = str.indexOf(':')
+    val className = str.slice(0 until index).trim('"')
+
+    return Pair(index, className)
+}
+
+fun checkIntOrString(value: Any): String {
+    return when (value.toString().toDoubleOrNull()) {
+        null -> "String"
+        else -> "Int"
     }
 }
