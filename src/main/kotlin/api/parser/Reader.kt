3b1cdb6d153c67435bc02f5b82e738d625690614
pichupido <ilyasov060@gmail.com>
2023-07-14 00:12:18 +0300

    Решена проблема с неверным форматом json-файла (фигурные скобки в начале и в конце файла)

--- src/main/kotlin/api/parser/Reader.kt
+++ src/main/kotlin/api/parser/Reader.kt
@@ -4,7 +4,9 @@ import java.io.File
 
 class Reader {
     fun readFile(fileName: String): String {
-        val str = File(fileName).readText(Charsets.UTF_8)
+        var str = File(fileName).readText(Charsets.UTF_8)
+        str = str.slice(1 until str.length - 1).trimIndent()
+
         return str.ifEmpty { error("Empty file") }
     }
 }
