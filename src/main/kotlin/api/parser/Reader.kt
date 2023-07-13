0f8c91c2fd2d7920080d7ad9d128a5b0fce83449
pichupido <ilyasov060@gmail.com>
2023-07-14 00:45:41 +0300

    FIX: вопрос с фигурными скобками в начале и конце json - более универсальное решение

--- src/main/kotlin/api/parser/Reader.kt
+++ src/main/kotlin/api/parser/Reader.kt
@@ -5,7 +5,8 @@ import java.io.File
 class Reader {
     fun readFile(fileName: String): String {
         var str = File(fileName).readText(Charsets.UTF_8)
-        str = str.slice(1 until str.length - 1).trimIndent()
+        val (firstIndex, lastIndex) = Pair(str.indexOf('{'), str.lastIndexOf('}'))
+        str = str.slice(firstIndex + 1 until lastIndex - 1).trimIndent()
 
         return str.ifEmpty { error("Empty file") }
     }
